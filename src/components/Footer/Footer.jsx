10cb7c5c1f8d65b15519b95a80bdea51ec77e4a7
fasil <faazilrahmaan@gmail.com>
2025-08-15 03:02:33 +0530

    typo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
         <li>Corparate Information</li>
         <li>Contact Us</li>
       </ul>
-      <p className="copyright-text">© 1997-2023 Netflix, Inc.</p>
+      <p className="copyright-text">© 1997-2025 Netflix, Inc.</p>
     </div>
   );
 };
